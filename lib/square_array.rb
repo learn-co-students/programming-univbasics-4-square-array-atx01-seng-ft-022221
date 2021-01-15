d0782ec936416562b74f603aff639d355b545e3a
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-15 01:25:53 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,16 +2,9 @@ def square_array(array)
   count = 0 
   new_array = []
   
-  while array[count] 
-  array[count]
-  count += 1 
+while count < array.length 
+puts new_array<< array[count] ** 2 
+count += 1 
 end
-
-array.each { |i| new_array<< i ** 2 }
-puts new_array
+new_array
 end
-
-
-original_num = [1, 2, 3]
-
-puts square_array(original_num)
