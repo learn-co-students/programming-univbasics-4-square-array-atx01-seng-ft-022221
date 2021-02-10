731b6c0db2e2d5cf39098df3a5039258532d453e
Robert Obi <robobi97@googlemail.com>
2021-02-10 22:41:40 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
+
 def square_array(array)
-  # your code here
-end
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array << array[i] * array[i]
+    i += 1
+  end
+  new_array
+end 
+
+
+
+
